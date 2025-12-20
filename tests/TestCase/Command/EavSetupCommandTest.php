c2135ae04cc0e414badf1498f4797beed8ec935f
Protelligence <paulm@protelligence.com>
2025-12-20 02:50:21 -0900

    EAV-9-setup-migration-infrastructure
    Implement EAV migration infrastructure with setup command and tests

---
+++ tests/TestCase/Command/EavSetupCommandTest.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace Eav\Test\TestCase\Command;
+
+use Cake\Console\TestSuite\ConsoleIntegrationTestTrait;
+use Cake\TestSuite\TestCase;
+
+class EavSetupCommandTest extends TestCase
+{
+    use ConsoleIntegrationTestTrait;
+
+    public function testDryRunOutputsMigration(): void
+    {
+        $this->exec('eav setup --dry-run --pk-type uuid --uuid-type uuid --json-storage json --connection test');
+        $this->assertExitSuccess();
+        $this->assertOutputContains('Dry run - migration not written.');
+        $this->assertOutputContains('class EavSetup');
+        $this->assertOutputContains('attributes');
+        $this->assertOutputContains('attribute_sets');
+    }
+}
